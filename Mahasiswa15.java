86653511a4e04a2186bb4ba526f907d2719d3661
Akmaaal-2121 <muhamadmiftahul21@gmail.com>
2025-02-18 14:30:50 +0700

    percobaan 1

--- Mahasiswa15.java
+++ Mahasiswa15.java
@@ -16,18 +16,34 @@ public class Mahasiswa15{
     }
 
     void updateIpk(double ipkBaru){
-        ipk = ipkBaru;
-    }
-
+            if(ipkBaru <= 4.0 && ipkBaru >= 0.0){
+                ipk = ipkBaru;
+            }else{
+                System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+            }
+        }
+    
     String nilaiKinerja(){
         if (ipk >= 3.5){
             return "Kinerja sangat baik";
         }else if (ipk >= 3.0){
-            return "Kinerja baik";
+                return "Kinerja baik";
         }else if (ipk >= 2.0){
             return "Kinerja cukup";
         }else{
             return "Kinerja kurang";
         }
+            
+        }
+    
+    public static void main(String[] args) {
+        Mahasiswa15 mhs1 = new Mahasiswa15();
+        mhs1.nama = "Akmal";
+        mhs1.nim = "244107060048";
+        mhs1.kelas = "1G";
+        mhs1.ipk = 3.8;
+
+        mhs1.updateIpk(4.5);
+        
     }
 }
